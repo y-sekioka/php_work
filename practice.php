c351032fc4ef1e710f0f03d35221f5605ef92d75
Y.SEKIOKA <y.sekioka84@gmail.com>
2019-06-13 12:03:50 +0000

    php_work

---
+++ practice.php
@@ -0,0 +1,43 @@
+<?php
+//課題１
+$a = 3;
+$b = 7;
+echo $a+$b." ";
+//課題１
+
+//課題2
+$array_month = ["1月","２月","３月","４月","５月","６月","７月","8月","９月","10月","11月","12月"];
+echo $array_month[7]." ";
+//課題2
+
+//課題3
+$hello = "Hello,";
+$name ="Sekioka";
+$world ="'s World!";
+echo $hello.$name.$world;
+//課題3
+
+//課題4
+$tech_boost = "tech";
+$tech_boost .=" boost";
+echo $tech_boost." ";
+//課題4
+
+//課題５
+$_2018_calender = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月",
+];
+// 12月を表示する
+echo $_2018_calender["December"];
+
